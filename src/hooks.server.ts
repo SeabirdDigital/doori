7813146355ba9dbb41979b07bd51ad44c161a18d
Filip Mårtensson <fenwikk@protonmail.com>
2023-07-05 22:13:19 -0400

    Removed dev ip change

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,7 @@
-import { SECRET_LOCAL_IP } from "$env/static/private";
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.ip = SECRET_LOCAL_IP ?? event.getClientAddress();
+	event.locals.ip = event.getClientAddress();
 
 	return await resolve(event);
 };
